8755e00b743c058c1715effb3dce87ce87dd8ac9
didam00 <didam00@naver.com>
2025-05-30 00:34:16 +0900

    update layout.tsx

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   domains: ['i.ytimg.com'],
-  // },
+  images: {
+    domains: ['i.ytimg.com'],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
